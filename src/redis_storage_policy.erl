c0a279c3b1c7509e873bdc490a4048bb447e449b
Gavin M. Roy <gavinmroy@gmail.com>
2014-06-29 21:09:27 -0400

    Rename and update the policy validator to call the worker

--- src/redis_storage_policy.erl
+++ src/redis_storage_policy.erl
@@ -6,7 +6,7 @@
 %% @doc define the runtime parameters and validators to setup policies
 %% @end
 
--module(redis_storage_parameters).
+-module(redis_storage_policy).
 
 -behaviour(rabbit_policy_validator).
 
@@ -14,12 +14,12 @@
          unregister/0,
          validate_policy/1]).
 
--define(RUNTIME_PARAMETERS,
+-define(POLICY_PARAMETERS,
         [{policy_validator,  <<"redis-storage-host">>},
          {policy_validator,  <<"redis-storage-port">>}]).
 
 -rabbit_boot_step({?MODULE,
-                   [{description, "Redis Storage Exchange Parameter Validation"},
+                   [{description, "Redis Storage Exchange Policy Validation"},
                     {mfa, {?MODULE, register, []}},
                     {requires, rabbit_registry},
                     {cleanup, {?MODULE, unregister, []}},
@@ -27,21 +27,21 @@
 
 register() ->
   [rabbit_registry:register(Class, Name, ?MODULE) ||
-    {Class, Name} <- ?RUNTIME_PARAMETERS],
+    {Class, Name} <- ?POLICY_PARAMETERS],
   ok.
 
 unregister() ->
   [rabbit_registry:unregister(Class, Name) ||
-    {Class, Name} <- ?RUNTIME_PARAMETERS],
+    {Class, Name} <- ?POLICY_PARAMETERS],
   ok.
 
 validate_policy(KeyList) ->
-  Host       = proplists:get_value(<<"redis-storage-host">>, KeyList, none),
-  Port       = proplists:get_value(<<"redis-storage-port">>, KeyList, none),
-  Validation = [redis_storage_lib:validate_host(Host),
-                redis_storage_lib:validate_port(Port)],
-  case Validation of
-    [ok, ok]                   -> ok;
-    [{error, Error, Args}, _]  -> {error, Error, Args};
-    [ok, {error, Error, Args}] -> {error, Error, Args}
+  Host = proplists:get_value(<<"redis-storage-host">>, KeyList, none),
+  Port = proplists:get_value(<<"redis-storage-port">>, KeyList, none),
+  case gen_server:call(redis_storage_worker, {validate_policy, Host, Port}) of
+    ok -> ok;
+    {error, Error} ->
+      {error,
+       "Could not connect to Redis at ~s port ~p: ~p", 
+       [Host, Port, Error]}
   end.
